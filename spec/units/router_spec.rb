a0cf4520b2f68e493688f741525b1f276eef5c0d
maedi <maediprichard@gmail.com>
2025-12-21 17:06:36 +1100

    Test that request events are sent to the router

--- spec/units/router_spec.rb
+++ spec/units/router_spec.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 require 'observers'
+require 'low_event' # RequestEvent. TODO: Move to LowLoop.
 require_relative '../../lib/router'
+require_relative '../factories/request_factory'
 
 RSpec.describe RainRouter do
   subject(:rain_router) { described_class.new }
@@ -12,8 +14,20 @@ RSpec.describe RainRouter do
     allow(Observers::Observables).to receive(:upsert)
   end
 
-  it 'defines routes as observable' do
-    rain_router.get '/user'
-    expect(Observers::Observables).to have_received(:upsert)
+  context 'when defining routes' do
+    it 'defines routes as observable' do
+      rain_router.get '/user'
+      expect(Observers::Observables).to have_received(:upsert)
+    end
+  end
+
+  context 'when handling events' do
+    let(:request_event) { Low::RequestEvent.new(request:) }
+    let(:request) { Low::RequestFactory.request(path: '/user') }
+
+    it 'converts a request event to a route event' do
+      rain_router.get '/user'
+      RainRouter.handle(event: request_event)
+    end
   end
 end
